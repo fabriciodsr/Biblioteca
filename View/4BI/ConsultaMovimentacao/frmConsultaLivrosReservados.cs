d6deac34529e2a039037c6a1d059fd18e17a4c01
LeyZem <tdb_mc@hotmail.com>
2015-11-21 11:25:33 -0200

    Forms Criadas

---
+++ View/4BI/ConsultaMovimentacao/frmConsultaLivrosReservados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View._4BI.ConsultaMovimentacao
+{
+    public partial class frmConsultaLivrosReservados : Form
+    {
+        public frmConsultaLivrosReservados()
+        {
+            InitializeComponent();
+        }
+    }
+}

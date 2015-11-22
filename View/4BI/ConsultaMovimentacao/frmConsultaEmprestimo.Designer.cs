e1621390c19fb581fa5af7641de0ffbada104dcd
Cristimasiero <cristimasiero@hotmail.com>
2015-11-22 18:30:22 -0200

    telas de consultas

--- View/4BI/ConsultaMovimentacao/frmConsultaEmprestimo.Designer.cs
+++ View/4BI/ConsultaMovimentacao/frmConsultaEmprestimo.Designer.cs
@@ -28,20 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConsultaEmprestimo));
+            this.cmbPesquisa = new System.Windows.Forms.ComboBox();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.dtgLivros = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgLivros)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cmbPesquisa
+            // 
+            this.cmbPesquisa.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPesquisa.FormattingEnabled = true;
+            this.cmbPesquisa.Items.AddRange(new object[] {
+            "TODOS",
+            "ID",
+            "TITULO"});
+            this.cmbPesquisa.Location = new System.Drawing.Point(258, 57);
+            this.cmbPesquisa.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbPesquisa.Name = "cmbPesquisa";
+            this.cmbPesquisa.Size = new System.Drawing.Size(133, 24);
+            this.cmbPesquisa.TabIndex = 139;
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(415, 58);
+            this.txtPesquisa.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(276, 22);
+            this.txtPesquisa.TabIndex = 140;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(86, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 20);
+            this.label2.TabIndex = 144;
+            this.label2.Text = "Opção de Busca";
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProcurar.BackColor = System.Drawing.Color.Transparent;
+            this.btnProcurar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnProcurar.BackgroundImage")));
+            this.btnProcurar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnProcurar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProcurar.FlatAppearance.BorderSize = 0;
+            this.btnProcurar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnProcurar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnProcurar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProcurar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnProcurar.Location = new System.Drawing.Point(702, 37);
+            this.btnProcurar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(67, 62);
+            this.btnProcurar.TabIndex = 141;
+            this.btnProcurar.UseVisualStyleBackColor = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancelar.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancelar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCancelar.BackgroundImage")));
+            this.btnCancelar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnCancelar.Location = new System.Drawing.Point(18, 94);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(67, 62);
+            this.btnCancelar.TabIndex = 143;
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // dtgLivros
+            // 
+            this.dtgLivros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgLivros.Location = new System.Drawing.Point(16, 166);
+            this.dtgLivros.Margin = new System.Windows.Forms.Padding(4);
+            this.dtgLivros.Name = "dtgLivros";
+            this.dtgLivros.Size = new System.Drawing.Size(1012, 363);
+            this.dtgLivros.TabIndex = 142;
             // 
             // frmConsultaEmprestimo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.ClientSize = new System.Drawing.Size(1045, 567);
+            this.Controls.Add(this.cmbPesquisa);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnProcurar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.dtgLivros);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmConsultaEmprestimo";
             this.Text = "CONSULTAR EMPRÉSTIMOS";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgLivros)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbPesquisa;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.DataGridView dtgLivros;
     }
 }

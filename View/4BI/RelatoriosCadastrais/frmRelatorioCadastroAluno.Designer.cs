546a0b7818b6e5b8aefedc19efb401b8a46f7088
Cristimasiero <cristimasiero@hotmail.com>
2015-11-23 01:45:06 -0200

    Aba Sobre

--- View/4BI/RelatoriosCadastrais/frmRelatorioCadastroAluno.Designer.cs
+++ View/4BI/RelatoriosCadastrais/frmRelatorioCadastroAluno.Designer.cs
@@ -35,11 +35,11 @@
             this.label3 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnPesquisarAluno = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.dtgReservas = new System.Windows.Forms.DataGridView();
-            this.btnPesquisarAluno = new System.Windows.Forms.Button();
-            this.txtNome = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dtgReservas)).BeginInit();
@@ -51,9 +51,11 @@
             this.groupBox1.Controls.Add(this.dateTimePicker2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.dateTimePicker1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(293, 80);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(391, 98);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Por Período:";
@@ -62,36 +64,40 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(179, 25);
+            this.label1.Location = new System.Drawing.Point(239, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.Size = new System.Drawing.Size(96, 20);
             this.label1.TabIndex = 39;
             this.label1.Text = "Data Final";
             // 
             // dateTimePicker2
             // 
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker2.Location = new System.Drawing.Point(166, 44);
+            this.dateTimePicker2.Location = new System.Drawing.Point(221, 54);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(98, 20);
+            this.dateTimePicker2.Size = new System.Drawing.Size(129, 22);
             this.dateTimePicker2.TabIndex = 38;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(49, 25);
+            this.label3.Location = new System.Drawing.Point(65, 31);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.Size = new System.Drawing.Size(105, 20);
             this.label3.TabIndex = 37;
             this.label3.Text = "Data Inicial";
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(36, 44);
+            this.dateTimePicker1.Location = new System.Drawing.Point(48, 54);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(98, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(129, 22);
             this.dateTimePicker1.TabIndex = 1;
             // 
             // groupBox2
@@ -100,42 +106,14 @@
             this.groupBox2.Controls.Add(this.txtNome);
             this.groupBox2.Controls.Add(this.radioButton3);
             this.groupBox2.Controls.Add(this.radioButton1);
-            this.groupBox2.Location = new System.Drawing.Point(311, 12);
+            this.groupBox2.Location = new System.Drawing.Point(415, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(437, 80);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(583, 98);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Classificar por:";
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(97, 34);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(49, 17);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "autor";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(33, 34);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(58, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "gênero";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // dtgReservas
-            // 
-            this.dtgReservas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgReservas.Location = new System.Drawing.Point(8, 113);
-            this.dtgReservas.Name = "dtgReservas";
-            this.dtgReservas.Size = new System.Drawing.Size(764, 336);
-            this.dtgReservas.TabIndex = 102;
             // 
             // btnPesquisarAluno
             // 
@@ -144,31 +122,68 @@
             this.btnPesquisarAluno.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPesquisarAluno.BackgroundImage")));
             this.btnPesquisarAluno.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnPesquisarAluno.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnPesquisarAluno.Location = new System.Drawing.Point(404, 28);
+            this.btnPesquisarAluno.Location = new System.Drawing.Point(539, 34);
+            this.btnPesquisarAluno.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPesquisarAluno.Name = "btnPesquisarAluno";
-            this.btnPesquisarAluno.Size = new System.Drawing.Size(26, 21);
+            this.btnPesquisarAluno.Size = new System.Drawing.Size(35, 26);
             this.btnPesquisarAluno.TabIndex = 109;
             this.btnPesquisarAluno.UseVisualStyleBackColor = false;
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(166, 25);
+            this.txtNome.Location = new System.Drawing.Point(221, 31);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNome.MaxLength = 50;
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(223, 20);
+            this.txtNome.Size = new System.Drawing.Size(296, 22);
             this.txtNome.TabIndex = 108;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(129, 42);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(62, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "autor";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(44, 42);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "gênero";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // dtgReservas
+            // 
+            this.dtgReservas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgReservas.Location = new System.Drawing.Point(11, 139);
+            this.dtgReservas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtgReservas.Name = "dtgReservas";
+            this.dtgReservas.Size = new System.Drawing.Size(1019, 414);
+            this.dtgReservas.TabIndex = 102;
             // 
             // frmRelatorioCadastroAluno
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.ClientSize = new System.Drawing.Size(1045, 567);
             this.Controls.Add(this.dtgReservas);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmRelatorioCadastroAluno";
             this.Text = "RELATÓRIO DE CADASTRO DE ALUNOS";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmRelatorioCadastroAluno_FormClosed);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
--- View/Properties/Resources.Designer.cs
+++ View/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace View.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap CAPA111 {
+            get {
+                object obj = ResourceManager.GetObject("CAPA111", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Entregar_Livro {
             get {
                 object obj = ResourceManager.GetObject("Entregar Livro", resourceCulture);
